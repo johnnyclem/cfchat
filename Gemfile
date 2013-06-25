0dd669ca0dc445b1a74bd0b4ffa09ba23e55d570
John Clem <johnnyclem@gmail.com>
2013-06-25 14:35:25 -0700

    added beautiful scaffold

--- Gemfile
+++ Gemfile
@@ -9,6 +9,20 @@ group :assets do
   gem 'uglifier', '>= 1.0.3'
 end
 
-gem 'jquery-rails'
+group :development do
+  gem "better_errors"
+  gem "binding_of_caller"
+end
+
+gem 'beautiful_scaffold'
+gem "jquery-rails", "2.3.0"
 gem 'unicorn'
 gem 'capistrano'
+gem "will_paginate"
+gem "ransack"
+gem "prawn", "1.0.0.rc1"
+gem "RedCloth"
+gem "bb-ruby"
+gem "bluecloth"
+gem "rdiscount"
+gem "sanitize"
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 Cfchat::Application.routes.draw do
 
-  resources :posts
+  match "posts/search_and_filter" => "posts#index", :via => [:get, :post], :as => :search_posts
+  resources :posts do
+    collection do
+      post :batch
+      get  :treeview
+    end
+    member do
+      post :treeview_update
+    end
+  end
 
-  root :to => 'posts#index'
+
+  root :to => 'beautiful#dashboard'
+  match ':model_sym/select_fields' => 'beautiful#select_fields'
 
 end
